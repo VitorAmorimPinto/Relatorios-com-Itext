939403826090535e93689be00a0319ce3efaebb8
Vitor Amorim <vitor232596.job@gmail.com>
2020-11-27 11:30:58 -0300

    Relatorio Coordenadores Adicionado

---
+++ Model/CoordenadorModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototipoRelatorio.Model
+{
+    public class CoordenadorModel
+    {
+        public int IdCurso { get; set; }
+        public int QtdAvaliacoes { get; set; }
+        public string NomeCoordenador{get; set; }
+        public string NomeCurso { get; set; }
+        public string Questao { get; set; }
+        public double  MediaQuestao { get; set; }
+    }
+}
